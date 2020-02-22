a330e7117c965b4225a3f67cb3f7047ddf2fedc0
tinaba <inaba1115@gmail.com>
2020-02-23 05:24:16 +0900

    impl

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,57 @@
 import * as BABYLON from 'babylonjs';
 
+class Mandelbulb {
+    private size: number;
+    private n_iter: number;
+    private m: number;
+    private threshold: number = 1000**30;
+
+    constructor(size: number, n_iter: number, m: number) {
+        this.size = size;
+        this.n_iter = n_iter;
+        this.m = m;
+    }
+
+    do_mandelbulb(c: BABYLON.Vector3): boolean {
+        let v = c.clone();
+        for (let i = 0; i < this.n_iter; i++) {
+            const r = v.length();
+            const phi = Math.atan2(v.y, v.x);
+            const theta = Math.atan2(Math.sqrt(v.x**2 + v.y**2), v.z);
+            const vr = Math.pow(r, this.m);
+            const vx = Math.sin(this.m * theta) * Math.cos(this.m * phi);
+            const vy = Math.sin(this.m * theta) * Math.sin(this.m * phi);
+            const vz = Math.cos(this.m * theta);
+            v = (new BABYLON.Vector3(vx * vr, vy * vr, vz * vr)).add(c);
+            if (v.length() > this.threshold) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    i_to_vec(i: number): BABYLON.Vector3 {
+        const harf = Math.floor(this.size/2);
+        let x = i % this.size - harf;
+        let y = Math.floor((i / this.size) % this.size) - harf;
+        let z = Math.floor(i / (this.size**2)) - harf;
+        return new BABYLON.Vector3(x, y, z);
+    }
+
+    render(scene: BABYLON.Scene): void {
+        let count = 0;
+        for (let i = 0; i < this.size**3; i++) {
+            const vec = this.i_to_vec(i);
+            if (this.do_mandelbulb(vec)) {
+                const x = BABYLON.MeshBuilder.CreateBox("box", {}, scene);
+                x.position = vec;
+                count += 1
+            }
+        }
+        console.log(count);
+    }
+}
+
 class Game {
     private _canvas: HTMLCanvasElement;
     private _engine: BABYLON.Engine;
@@ -7,47 +59,27 @@ class Game {
     private _camera: BABYLON.FreeCamera;
     private _light: BABYLON.Light;
 
-    constructor(canvasElement : string) {
-        // Create canvas and engine.
+    constructor(canvasElement: string) {
         this._canvas = document.getElementById(canvasElement) as HTMLCanvasElement;
         this._engine = new BABYLON.Engine(this._canvas, true);
     }
 
-    createScene() : void {
-        // Create a basic BJS Scene object.
+    createScene(): void {
         this._scene = new BABYLON.Scene(this._engine);
 
-        // Create a FreeCamera, and set its position to (x:0, y:5, z:-10).
-        this._camera = new BABYLON.FreeCamera('camera1', new BABYLON.Vector3(0, 5,-10), this._scene);
-
-        // Target the camera to scene origin.
+        this._camera = new BABYLON.FreeCamera('camera1', new BABYLON.Vector3(-100, -100, -100), this._scene);
         this._camera.setTarget(BABYLON.Vector3.Zero());
-
-        // Attach the camera to the canvas.
         this._camera.attachControl(this._canvas, false);
 
-        // Create a basic light, aiming 0,1,0 - meaning, to the sky.
-        this._light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(0,1,0), this._scene);
+        this._light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(0, 1, 0), this._scene);
 
-        // Create a built-in "sphere" shape; with 16 segments and diameter of 2.
-        let sphere = BABYLON.MeshBuilder.CreateSphere('sphere1',
-                                {segments: 16, diameter: 2}, this._scene);
-
-        // Move the sphere upward 1/2 of its height.
-        sphere.position.y = 1;
-
-        // Create a built-in "ground" shape.
-        let ground = BABYLON.MeshBuilder.CreateGround('ground1',
-                                {width: 6, height: 6, subdivisions: 2}, this._scene);
+        (new Mandelbulb(201, 3, 8)).render(this._scene);
     }
 
-    doRender() : void {
-        // Run the render loop.
+    doRender(): void {
         this._engine.runRenderLoop(() => {
             this._scene.render();
         });
-
-        // The canvas/window resize event handler.
         window.addEventListener('resize', () => {
             this._engine.resize();
         });
@@ -55,12 +87,7 @@ class Game {
 }
 
 window.addEventListener('DOMContentLoaded', () => {
-    // Create the game using the 'renderCanvas'.
     let game = new Game('renderCanvas');
-
-    // Create the scene.
     game.createScene();
-
-    // Start render loop.
     game.doRender();
 });
